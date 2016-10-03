f727c66b1c8614afcfc627a3b95828fe82eee1d7
TiNJiM <brannorth04@aol.com>
2016-10-03 12:47:31 -0600

    Final

---
+++ src/Tester.java
@@ -0,0 +1,10 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle cir1 = new Circle(35.5);
+		System.out.println (cir1.diameter());
+	}
+
+}
